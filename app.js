abb47b01d43da328693dbf37a485b29c5c07dfd7
tejasvishrivastava43@gmail.com <tejasvishrivastava43@gmail.com>
2022-09-09 13:46:02 +0530

    commit message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 var item = require('./items')
 const app = express()
 var cors = require('cors')
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 3001;
 app.use(express.json())
 app.use(cors())
 app.use(express.static("public"))
